b6109d7908e78a7de065c26660a074638c1e0b24
janarpe <jan.arpe@gmx.de>
2015-12-27 01:30:01 +0100

    Initial Start

---
+++ js/data.js
@@ -0,0 +1,17 @@
+var glad_counter = 0;
+    function glad() {
+        glad_counter += 1;
+        document.getElementById("glad").innerHTML = glad_counter;
+    };
+
+var sad_counter = 0;
+    function sad() {
+        sad_counter += 1;
+        document.getElementById("sad").innerHTML = sad_counter;
+    };
+
+var mad_counter = 0;
+    function mad() {
+        mad_counter += 1;
+        document.getElementById("mad").innerHTML = mad_counter;
+    };
