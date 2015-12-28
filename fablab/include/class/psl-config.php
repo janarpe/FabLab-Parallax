286129e013b61c77a227df1877614f3e6c8da5fe
janarpe <jan.arpe@gmx.de>
2015-12-28 02:50:00 +0100

    Included Login

---
+++ fablab/include/class/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Das sind die Login-Angaben für die Datenbank
+ */  
+define("HOST", "db606864190.db.1and1.com");     // Der Host mit dem du dich verbinden willst.
+define("USER", "dbo606864190");    // Der Datenbank-Benutzername. 
+define("PASSWORD", "XVtu3XuPwrXhFPUd!");    // Das Datenbank-Passwort. 
+define("DATABASE", "db606864190");    // Der Datenbankname.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // NUR FÜR DIE ENTWICKLUNG!!!!
